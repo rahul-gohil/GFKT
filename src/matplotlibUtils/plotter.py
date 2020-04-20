f598622afb5e0e644a9d79292f015fc4b7944326
Rahul Gohil <rahul.gohil@workmail.com>
2020-04-20 21:41:23 +0530

    Renaming

--- src/matplotlibUtils/plotter.py
+++ src/matplotlibUtils/plotter.py
@@ -8,17 +8,17 @@ from shapes.line         import lines
 
 def plot(n, init):
     plotAxes(init[0].x, init[0].y)
-    pointsX = [init[0].x, init[1].x] + list(map(
+    X = [init[0].x, init[1].x] + list(map(
         lambda i : points[i].x,
         range(n)
     ))
-    pointsY = [init[0].y, init[1].y] + list(map(
+    Y = [init[0].y, init[1].y] + list(map(
         lambda i : points[i].y,
         range(n)
     ))
-    plotLinePrime(n, pointsX, pointsY)
+    plotLinePrime(n, X, Y)
     plotLine(n, lines)
     plotSpiral(n, Spiral())
-    plt.scatter(pointsX, pointsY)
+    plt.scatter(X, Y)
     plt.show()
     
